ec53a7dfc47a5409e2bfc249c2d110353f28d019
joseph_scoggins <borninthecaul@gmail.com>
2016-10-24 23:40:23 -0700

    first three

---
+++ format_string1.py
@@ -0,0 +1,21 @@
+_id = 8964
+_dept = 'PF'
+_store = 'CHC'
+_age = 39
+_work = 'NSM'
+_zip = 97220
+_height_inch = 66.00
+_height_cent = _height_inch * 2.54
+_weight_pound = 155.0000
+_weight_kilo = _weight_pound * 0.4536
+
+print ("I work at %s," % _work)
+print ("in the %s department." % _dept)
+print ("My employee ID is %d." % _id)
+print ("I drive all the way to %s" % _store)
+print ("from hazelwood, %d." % _zip)
+print ("I can't believe I'm %d years old." % _age)
+print ("I'm still %d inches tall;" % _height_inch)
+print ("that's %f centimeters." % _height_cent)
+print ("I weigh about %d pounds, I hope;" % _weight_pound)
+print ("that's %f kilograms" % _weight_kilo)
