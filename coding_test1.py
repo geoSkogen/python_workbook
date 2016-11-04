ff527dd6515762aa0eaf6f4e33a556864be10d85
joseph_scoggins <borninthecaul@gmail.com>
2016-11-03 23:58:12 -0700

    add search function

--- coding_test1.py
+++ coding_test1.py
@@ -3,68 +3,85 @@ class Catalog(object):
     def __init__(self, songs):
         self.songs = songs
 
+    def start_song(self, index):
+        asong = self.songs[index]
+        for i in asong:
+            print(asong[i])
+
 class Song(object):
 
     def __init__(self, lyrics):
         self.lyrics = lyrics
 
-
-def begin_entry():
+#this in the main method--defines main variables, objects, etc.
+def init_funcs():
     playlist = []
     catalog = Catalog(playlist)
     prompt = "> "
+
+    def begin_entry():
     #these are nested methods; flow control begins below
-    def enter_lines():
-        lines = []
-        print("enter title")
-        lines.append(input(prompt))
-        print("enter line by line")
-        i = 1
-        lines.append(input(prompt))
-        while lines[i] != "end":
-            i += 1
+        def enter_lines():
+            lines = []
+            print("enter title")
             lines.append(input(prompt))
-        if lines[i] == "end":
-            print(lines)
-            next_song = Song(lines)
-            catalog.songs.append(lines)
-            begin_entry()
+            print("enter line by line")
+            i = 1
+            lines.append(input(prompt))
+            while lines[i] != "end":
+                i += 1
+                lines.append(input(prompt))
+                if lines[i] == "end":
+                    print(lines)
+                    next_song = Song(lines)
+                    catalog.songs.append(lines)
+                    begin_entry()
 
-    def search_by_title(thistitle):
-        titlefound = False
-        for i in catalog.songs:
-            if i[0] == thistitle:
-                titlefound = True
-                print(i)
+
+        def search_by_title(thistitle, erase):
+            titlefound = False
+            count = -1;
+            for i in catalog.songs:
+                count += 1
+                if i[0] == thistitle:
+                    titlefound = True
+                    if erase == "D":
+                        i = []
+                        print("song deleted")
+                        begin_entry()
+                    print(catalog.songs)
+                    #catalog.start_song(count)
+                    begin_entry()
+            if titlefound == False:
+                print("no matches found for %s" % thistitle)
                 begin_entry()
-        if titlefound == False:
-            print("no matches found for %s" % thistitle)
-            begin_entry()
 
-    def get_title():
-        serchtitle = input(prompt)
-        search_by_title(searchtitle)        
+        def get_title(toast):
+            print("enter song title")
+            searchtitle = input(prompt)
+            search_by_title(searchtitle, toast)
 
-    def make_selection(char):
-        if char == "N":
-            enter_lines()
-        elif char == "V":
-            print(catalog.songs)
-            begin_entry()
-        elif char == "D":
-            print("delete coming soon")
-            begin_entry()
-        else:
-            print("whatup python?")
-            begin_entry()
+        def make_selection(char):
+            if char == "N":
+                enter_lines()
+            elif char == "V":
+                 get_title("V")
+#                print(catalog.songs)
+#                begin_entry()
+            elif char == "D":
+                get_title("D")
+                begin_entry()
+            else:
+                print("whatup python?")
+                begin_entry()
 
     #flow control begins here
-    print ("N = new song || V = view song || D = delete song")
-    selector =  input(prompt)
-    if selector == "N" or selector == "V" or selector == "D":
-        make_selection(selector)
-    else:
-        print("unrecognized character")
-        being_entry()
-
-begin_entry()
+        print ("N = new song || V = view song || D = delete song")
+        selector =  input(prompt)
+        if selector == "N" or selector == "V" or selector == "D":
+            make_selection(selector)
+        else:
+            print("unrecognized character")
+            begin_entry()
+    begin_entry()
+init_funcs()
